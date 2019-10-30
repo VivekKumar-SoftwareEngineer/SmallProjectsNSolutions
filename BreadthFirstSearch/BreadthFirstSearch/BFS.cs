26ff9bbd3a8d898b40958d5cd66aa5e44a8cb80e
Vivek Kumar <31600718+VivekKumar-SoftwareEngineer@users.noreply.github.com>
2019-10-29 20:55:46 -0700

    Breadth First Traversal !

---
+++ BreadthFirstSearch/BreadthFirstSearch/BFS.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BreadthFirstSearch
+{
+    internal class BFS
+    {
+        internal void BreadthFirstTraversal(Tree tree)
+        {
+            if (tree == null)
+                return;
+
+            Queue<Tree> queue = new Queue<Tree>();
+            queue.Enqueue(tree);
+
+            while (queue.Count() > 0)
+            {
+                Tree node = queue.Dequeue();
+                Console.Write(node.value + " ");
+                foreach (Tree child in node.children)
+                {
+                    if (child != null)
+                    {
+                        queue.Enqueue(child);
+                    }
+                }
+            }
+        }
+    }
+
+    internal class Tree
+    {
+        internal int value;
+        internal Tree[] children;
+
+        internal Tree(int value)
+        {
+            this.value = value;
+            children = new Tree[5];
+        }
+    }
+}
